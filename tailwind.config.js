48fb41a5a046c48a77928a76c11e8c8123feebad
raffi pasya <raffipasya4826@gmail.com>
2025-05-23 08:58:58 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // bisa 'media' atau 'class', kita pakai 'class'
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
